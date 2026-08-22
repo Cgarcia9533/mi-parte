713e6a630b68a4d1f3ef03032d0d4a0fb9982a88
Cgarcia9533 <cgarcia95@outlook.es>
2026-08-22 22:58:55 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'mi-parte-1.2-v5';
+const CACHE = 'mi-parte-1.2-v7';
 const ARCHIVOS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png', './icon-512-maskable.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ARCHIVOS)).then(() => self.skipWaiting()));
